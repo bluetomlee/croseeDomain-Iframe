b16847ad9525691adbf75b4ac93531b0e3883912
Bluetom <tomliyi@qq.com>
2014-11-12 10:58:54 +0800

    update

--- crossDomain.js
+++ crossDomain.js
@@ -1,13 +1,31 @@
-function crossDomain(url,callback)
-    { 
+function crossDomain(url,callback){
         var div = document.createElement('div');
-        div.innerHTML = " <div id=\"j_ajax_form\"><form id=\"j_ajax_inner_form\" name=\"j_ajax_inner_form\" method=\"post\"><input type=\"hidden\" id=\"j_ajax_inner_content\" name=\"j_ajax_inner_content\"></form><iframe id=\"j_ajax_iframe\" style=\"display:none\" height=\"1\" width=\"1\" name=\"j_ajax_iframe\"></iframe></div>";
+        div.innerHTML = "<div id=\"j_ajax_form\"><form id=\"j_ajax_inner_form\" name=\"j_ajax_inner_form\" method=\"post\"><input type=\"hidden\" id=\"j_ajax_inner_content\" name=\"j_ajax_inner_content\"></form><iframe id=\"j_ajax_iframe\" style=\"display:none\" height=\"1\" width=\"1\" name=\"j_ajax_iframe\"></iframe></div>";
         document.body.appendChild(div); 
         var frm = document.forms["j_ajax_inner_form"];
         var ifr = document.getElementById("j_ajax_iframe"); 
-        frm.action = url; 
+        var callbackName = ('jsonp_' + Math.random()).replace(".", "");
+        frm.action = url + "&dataType=iframe&callback=top."+callbackName;
         frm.target = "j_ajax_iframe"; 
-        frm.submit();  
+        frm.submit();
+        
+        if (callback != null){
+            var script = document.createElement("script");
+            script.setAttribute("type", "text/javascript");
+            script.id = callbackName;
+            document.getElementById("j_ajax_iframe").appendChild(script);
+            window[callbackName] = function(json){
+                var script = document.getElementById(callbackName);
+                document.getElementById("j_ajax_iframe").removeChild(script);
+                callback(json);
+            }
+            return callbackName;
+        } 
+        // window[callbackName] = function (json) {
+  //           window[callbackName] = null;
+        //  callback(json);
+  //       };
+        /* 
         if(ifr.attachEvent){ //  ie
             ifr.attachEvent("onload", function(){              
                 var str = ifr.contentWindow; 
@@ -18,23 +36,15 @@ function crossDomain(url,callback)
 
         }else{
             ifr.onload = function(){ 
-                var str = ifr.contentWindow; 
-                // alert(str.document.body.innerHTML); 
-                ifr.src = "about:blank"; 
-                ifr.onload = null; 
+                var str = ifr.contentWindow;
+                // alert(str.document.body.innerHTML);
+                ifr.src = "about:blank";
+                ifr.onload = null;
             }
 
         }
-        if (callback != null){
-            var random_name = "cb_" + Math.random().toString();
-                window[random] = function(names){
-                    var scr = document.getElementById(random);
-                    document.getElementByTagName("head")[0].removeChild(script);
-                    callback(names);
-                }
-                return random;
-
-        }    
+         */
+        //if (callback != null){callback();}    
         return false; 
 
 }
